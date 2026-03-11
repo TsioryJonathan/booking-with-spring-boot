4e2089f93b39a915af0710a0ae5438327c099e7d
Jonathan <rakotoarisoajonathan0@gmail.com>
2026-03-11 17:04:24 +0300

    refactor: use @RestControllerAdvice instead of @ControllerAdvice

--- src/main/java/org/spring/prog2finalexamwithspring/Advice/GlobalExceptionHandler.java
+++ src/main/java/org/spring/prog2finalexamwithspring/Advice/GlobalExceptionHandler.java
@@ -4,10 +4,10 @@ import org.spring.prog2finalexamwithspring.Exception.RoomAlreadyBookedException;
 import org.spring.prog2finalexamwithspring.Exception.RoomNumberNotValidException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(RoomAlreadyBookedException.class)
     public ResponseEntity<String> handleRoomAlreadyBookedException(RoomAlreadyBookedException ex) {
